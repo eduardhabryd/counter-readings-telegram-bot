982cb706e45e2feca8fffdee9a3ab662effdf7b4
eduardhabryd <eduardhabryd@gmail.com>
2023-12-05 16:32:16 +0200

    create utils.py for utils

---
+++ utils.py
@@ -0,0 +1,32 @@
+from aiogram.fsm.state import State, StatesGroup
+
+
+class Form(StatesGroup):
+    lang = State()
+    address = State()
+    account = State()
+
+
+class Language:
+    def __init__(self, lang):
+        self.lang = lang
+
+    def get_lang(self):
+        return self.lang
+
+    def set_lang(self, lang):
+        self.lang = lang
+
+
+messages_text = {
+    "eng": {
+        "address": "Please provide your address:",
+        "account": "Please provide your personal account number:",
+        "greetings": "Nice to meet you!",
+    },
+    "ukr": {
+        "address": "Введіть вашу адресу:",
+        "account": "Введіть ваш номер особового рахунку:",
+        "greetings": "Доброго дня!",
+    }
+}
