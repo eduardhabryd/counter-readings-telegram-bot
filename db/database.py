69f142eb9000a9c9e144cf7e99aa0425695a27f2
eduardhabryd <eduardhabryd@gmail.com>
2023-12-05 19:05:33 +0200

    modified to be async

--- db/database.py
+++ db/database.py
@@ -1,6 +1,11 @@
-from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = 'sqlite:///example.db'
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+DATABASE_URL = 'sqlite+aiosqlite:///example.db'
+engine = create_async_engine(DATABASE_URL, echo=True)
+
+SessionLocal = sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+)
