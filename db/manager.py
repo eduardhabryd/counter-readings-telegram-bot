f3b4ee88156f1e6994af7a50d1d7e546740482b7
eduardhabryd <eduardhabryd@gmail.com>
2023-12-05 19:35:39 +0200

    fix updating functionality

--- db/manager.py
+++ db/manager.py
@@ -9,8 +9,7 @@ async def get_user_by_telegram_id(telegram_user_id):
     async with SessionLocal() as session:
         stmt = select(User).where(User.telegram_user_id == telegram_user_id)
         result = await session.execute(stmt)
-
-        user = result.scalar()
+        user = result.scalar_one()
         return user
 
 
@@ -19,10 +18,15 @@ async def create_or_update_user(telegram_user_id, language, address, account):
         existing_user = await get_user_by_telegram_id(telegram_user_id)
 
         if existing_user is not None:
+            print("Existing User:", existing_user)
+            print("New Data:", language, address, account)
             existing_user.language = language
             existing_user.address = address
             existing_user.account = account
+
+            updated_user = await session.merge(existing_user)
         else:
+            print("Creating New User")
             new_user = User(
                 telegram_user_id=telegram_user_id,
                 language=language,
