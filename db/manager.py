30acbcd695558c98c07d0bc6d95af6fcf68088f0
eduardhabryd <eduardhabryd@gmail.com>
2023-12-05 19:05:43 +0200

    modified to be async

--- db/manager.py
+++ db/manager.py
@@ -1,26 +1,44 @@
-from db.database import SessionLocal, engine
-from db.models import Base, User, Counter
+from sqlalchemy.ext.asyncio import AsyncSession
+from db.database import SessionLocal
+from db.models import User, Counter
 
-Base.metadata.create_all(bind=engine)
-
-
-def create_user(telegram_user_id, language, address, account):
-    new_user = User(
-        telegram_user_id=telegram_user_id,
-        language=language,
-        address=address,
-        account=account
-    )
-    with SessionLocal() as session:
-        session.add(new_user)
-        session.commit()
+from sqlalchemy import select
 
 
-def create_counter(telegram_user_id, counter_image_url):
-    new_counter = Counter(
-        telegram_user_id=telegram_user_id,
-        counter_image_url=counter_image_url
-    )
-    with SessionLocal() as session:
+async def get_user_by_telegram_id(telegram_user_id):
+    async with SessionLocal() as session:
+        stmt = select(User).where(User.telegram_user_id == telegram_user_id)
+        result = await session.execute(stmt)
+
+        user = result.scalar()
+        return user
+
+
+async def create_or_update_user(telegram_user_id, language, address, account):
+    async with SessionLocal() as session:
+        existing_user = await get_user_by_telegram_id(telegram_user_id)
+
+        if existing_user is not None:
+            existing_user.language = language
+            existing_user.address = address
+            existing_user.account = account
+        else:
+            new_user = User(
+                telegram_user_id=telegram_user_id,
+                language=language,
+                address=address,
+                account=account
+            )
+            session.add(new_user)
+
+        await session.commit()
+
+
+async def create_counter(telegram_user_id, counter_image_url):
+    async with SessionLocal() as session:
+        new_counter = Counter(
+            telegram_user_id=telegram_user_id,
+            counter_image_url=counter_image_url
+        )
         session.add(new_counter)
-        session.commit()
+        await session.commit()
