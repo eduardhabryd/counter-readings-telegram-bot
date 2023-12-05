a2d39bf0cc3893ad8cdb9bce7c95b3b53edb5aa2
eduardhabryd <eduardhabryd@gmail.com>
2023-12-05 22:21:18 +0200

    remove redundant code

--- db/models.py
+++ db/models.py
@@ -1,6 +1,4 @@
-from datetime import datetime
-
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -17,12 +15,3 @@ class User(Base):
 
     def __str__(self):
         return f"User(id={self.id}, telegram_user_id={self.telegram_user_id}, language={self.language}, address={self.address}, account={self.account})"
-
-
-class Counter(Base):
-    __tablename__ = 'counters'
-
-    id = Column(Integer, primary_key=True)
-    telegram_user_id = Column(Integer)
-    created_at = Column(DateTime, default=datetime.now)
-    counter_image_url = Column(String)
