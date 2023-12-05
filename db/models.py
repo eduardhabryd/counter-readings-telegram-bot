a088460e9fb1e756d9fefac52ec0c45f1b9ae439
eduardhabryd <eduardhabryd@gmail.com>
2023-12-05 17:21:34 +0200

    implement User model and Counter model

---
+++ db/models.py
@@ -0,0 +1,25 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    telegram_user_id = Column(Integer, unique=True)
+    language = Column(String)
+    address = Column(String)
+    account = Column(String)
+
+    def __str__(self):
+        return f"User(id={self.id}, telegram_user_id={self.telegram_user_id}, language={self.language}, address={self.address}, account={self.account})"
+
+
+class Counter(Base):
+    __tablename__ = 'counters'
+
+    id = Column(Integer, primary_key=True)
+    telegram_user_id = Column(Integer)
+    counter_image_url = Column(String)
