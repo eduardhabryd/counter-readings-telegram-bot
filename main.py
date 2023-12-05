bb7b999b07b0a10e5aea84ec40c9ab72f54bf07f
eduardhabryd <eduardhabryd@gmail.com>
2023-12-05 19:06:18 +0200

    user memory storage to persist data across multiple messages

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import sys
 from os import getenv
 
 from aiogram.fsm.context import FSMContext
+from aiogram.fsm.storage.memory import MemoryStorage
 from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher, types
 from aiogram.enums import ParseMode
@@ -15,12 +16,15 @@ from aiogram.filters import Filter
 
 from utils import messages_text, Form, Language
 from filters import LangFilter
+from db.manager import create_or_update_user, create_counter
 
 load_dotenv()
 
 TOKEN = getenv("BOT_TOKEN")
 lang_type = Language("eng")
-dp = Dispatcher()
+
+storage = MemoryStorage()
+dp = Dispatcher(storage=storage)
 
 
 @dp.message(CommandStart())
@@ -43,6 +47,13 @@ async def language_state(message: Message, state: FSMContext) -> None:
     if message.text == "🇺🇦 Ukrainian":
         lang_type.set_lang("ukr")
 
+    await state.set_data(
+        {
+            "telegram_user_id": message.from_user.id,
+            "language": lang_type.get_lang()
+        }
+    )
+
     replies = messages_text[lang_type.get_lang()]
     greetings = replies["greetings"]
 
@@ -54,15 +65,23 @@ async def language_state(message: Message, state: FSMContext) -> None:
 @dp.message(Form.address)
 async def command_address(message: Message, state: FSMContext) -> None:
     replies = messages_text[lang_type.get_lang()]
-    print(message.text)
+    address = message.text
+    await state.update_data(address=address)
     await message.answer(replies["account"], reply_markup=types.ReplyKeyboardRemove())
     await state.set_state(Form.account)
 
 
 @dp.message(Form.account)
 async def account_state(message: Message, state: FSMContext) -> None:
+    data = await state.get_data()
+    telegram_user_id = data.get("telegram_user_id")
+    language = data.get("language")
+    address = data.get("address")
     account = message.text
-    print(account)
+    await create_or_update_user(telegram_user_id, language, address, account)
+    print(data, account)
+    replies = messages_text[lang_type.get_lang()]
+    await message.answer(replies["success"], reply_markup=types.ReplyKeyboardRemove())
     await state.clear()
 
 
